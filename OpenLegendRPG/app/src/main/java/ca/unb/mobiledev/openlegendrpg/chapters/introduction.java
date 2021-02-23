05a9ad23bef9cd6ee69eea68d9e4624bd42d7c79
Elizabeth <lizmercer@bellaliant.net>
2021-02-23 08:21:03 -0400

    made core rules work

---
+++ OpenLegendRPG/app/src/main/java/ca/unb/mobiledev/openlegendrpg/chapters/introduction.java
@@ -0,0 +1,46 @@
+package ca.unb.mobiledev.openlegendrpg.chapters;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import ca.unb.mobiledev.openlegendrpg.R;
+import com.journaldev.navigationdrawer.CoreRulesFragment;
+
+public class introduction extends android.app.Activity {
+
+    private Button ContentsButton;
+    private Button NextButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        Log.i("please: ", "onCreate() called");
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragement_intro);
+
+        ContentsButton = findViewById(R.id.TableButton);
+        ContentsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        NextButton = findViewById(R.id.NextButton);
+        NextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(introduction.this, chapter1.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
+
+
