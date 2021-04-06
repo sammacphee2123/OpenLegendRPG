8783dcd3cfc10a7bcc26d558b98ff53ed8cab206
VictorFernandes <victor.ferc@outlook.com>
2021-04-05 23:36:33 -0300

    completed ch6 content and changed a couple things in ch5

--- OpenLegendRPG/app/src/main/java/ca/unb/mobiledev/openlegendrpg/chapters/chapter6.java
+++ OpenLegendRPG/app/src/main/java/ca/unb/mobiledev/openlegendrpg/chapters/chapter6.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 import ca.unb.mobiledev.openlegendrpg.R;
 
@@ -12,10 +14,15 @@ public class chapter6  extends android.app.Activity{
     private Button ContentsButton;
     private Button NextButton;
     private Button PreviousButton;
+    private Button wealthTitle;
+    private Button carryingCapacityTitle;
+    private Button weaponsAndImplementsTitle;
+    private Button armorTitle;
+    private Button buildingWeaponsTitle;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragement_ch6);
+        setContentView(R.layout.fragment_ch6);
 
         ContentsButton = findViewById(R.id.TableButton);
         ContentsButton.setOnClickListener(new View.OnClickListener() {
@@ -44,6 +51,57 @@ public class chapter6  extends android.app.Activity{
                 finish();
             }
         });
+
+        wealthTitle = findViewById(R.id.wealthButton);
+        wealthTitle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView text = findViewById(R.id.ch6_wealth_title);
+                ScrollView sv = findViewById(R.id.scrl_ch6);
+                sv.scrollTo(0, text.getTop());
+            }
+        });
+
+        carryingCapacityTitle = findViewById(R.id.carryingCapacityButton);
+        carryingCapacityTitle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView text = findViewById(R.id.ch6_carrying_capacity_title);
+                ScrollView sv = findViewById(R.id.scrl_ch6);
+                sv.scrollTo(0, text.getTop());
+            }
+        });
+
+        weaponsAndImplementsTitle = findViewById(R.id.weaponsAndImplementsButton);
+        weaponsAndImplementsTitle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView text = findViewById(R.id.ch6_weapons_and_implements);
+                ScrollView sv = findViewById(R.id.scrl_ch6);
+                sv.scrollTo(0, text.getTop());
+            }
+        });
+
+        armorTitle = findViewById(R.id.armorButton);
+        armorTitle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView text = findViewById(R.id.ch6_armor_title);
+                ScrollView sv = findViewById(R.id.scrl_ch6);
+                sv.scrollTo(0, text.getTop());
+            }
+        });
+
+        buildingWeaponsTitle = findViewById(R.id.buildWeaponsButton);
+        buildingWeaponsTitle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView text = findViewById(R.id.ch6_building_weapons_title);
+                ScrollView sv = findViewById(R.id.scrl_ch6);
+                sv.scrollTo(0, text.getTop());
+            }
+        });
+
     }
 }
 
